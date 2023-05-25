4ef44dc48a0a0be4fb19e499719a09f732b29465
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-25 16:45:19 +0000

    Main monty program

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * main - Monty program code
+ * @argc: The Number of command line arguments passed
+ * @argv: Array of command line arguments passed
+ * Return: EXIT_SUCCESS on success and EXIT_FAILURE on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	stack_t *stack = NULL;
+	int line_num = 0;
+	char line[LINE_LENGTH];
+	char *opcode, *arg;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, LINE_LENGTH, file) != NULL)
+	{
+		line_num++;
+		opcode = strtok(line, " \t\n");
+		if(opcode == NULL || opcode[0] == '#')
+			continue;
+		arg = strtok(NULL, " \t\n");
+		if(!execute_opcode(opcode, arg, &stack, line_num))
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	free_stack(stack);
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
