d220d87a21d739cd444fad3ad98a273814980aac
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-26 01:38:26 +0000

    Struct manipulation

--- functions.c
+++ functions.c
@@ -12,7 +12,7 @@ void add_node(stack_t **stack, int n)
 	stack_t *temp;
 
 	temp = malloc(sizeof(stack_t));
-	if(temp == NULL)
+	if (temp == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
@@ -22,8 +22,42 @@ void add_node(stack_t **stack, int n)
 		temp->n = n;
 		temp->prev = NULL;
 		temp->next = *stack;
-		if(*stack != NULL)
+		if (*stack != NULL)
 			(*stack)->prev = temp;
 		*stack = temp;
 	}
+}
+
+#include "monty.h"
+
+/**
+ * add_node_end - Add a new node at the end of the stack.
+ * @stack: Double pointer to the stack.
+ * @n: Value to be stored in the new node.
+ * Return: void
+ */
+ 
+
+void add_node_end(stack_t **stack, int n)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *last = *stack;
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*stack == NULL)
+	{
+		new_node->prev = NULL;
+		*stack = new_node;
+		return;
+	}
+	while (last->next)
+		last = last->next;
+	last->next = new_node;
+	new_node->prev = last;
 }
